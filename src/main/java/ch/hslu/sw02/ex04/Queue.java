5fc749aef2784a1b26ddba3350045d0010dbdb3c
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2023-03-06 12:23:16 +0100

    queue with ring buffer

--- src/main/java/ch/hslu/sw02/ex04/Queue.java
+++ src/main/java/ch/hslu/sw02/ex04/Queue.java
@@ -1,4 +1,61 @@
 package ch.hslu.sw02.ex04;
 
-public class Queue {
+import java.util.Arrays;
+
+public class Queue<Item> {
+    private int capacity;
+    public int readPos  = -1;
+    public int writePos = -1;
+    private Item[] elements;
+
+    public Queue(int capacity) {
+        this.capacity = capacity;
+        elements = (Item[])new Object[capacity];
+    }
+
+    public void enqueue(Item item) {
+        incrementWritePos();
+        elements[writePos] = item;
+    }
+
+    public Item dequeue() {
+        incrementReadPos();
+        Item element = elements[readPos];
+        elements[readPos] = null;
+
+        if (readPos == writePos) {
+            readPos = -1;
+            writePos = -1;
+        }
+
+        return element;
+    }
+
+    public void incrementWritePos() {
+        writePos++;
+
+        if (writePos == capacity) {
+            writePos = 0;
+            if (readPos == -1) {
+                readPos = 0;
+            }
+            return;
+        }
+
+        if (writePos == readPos) {
+            incrementReadPos();
+        }
+    }
+
+    public void incrementReadPos() {
+        readPos++;
+
+        if (readPos == capacity) {
+            readPos = 0;
+        }
+    }
+
+    public String toString() {
+        return Arrays.toString(elements);
+    }
 }
---
+++ src/test/java/ch/hslu/sw02/ex04/QueueTest.java
@@ -0,0 +1,28 @@
+package ch.hslu.sw02.ex04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void incrementWritePos() {
+    }
+
+    @Test
+    void incrementReadPos() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
